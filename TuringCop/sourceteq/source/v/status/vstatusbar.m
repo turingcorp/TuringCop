4fa39ac67df8dc4fa9e1825b02c083eb3ae7cc1f
vauxhall <zero.griffin@gmail.com>
2016-02-08 15:12:25 -0600

    border

--- TuringCop/sourceteq/source/v/status/vstatusbar.m
+++ TuringCop/sourceteq/source/v/status/vstatusbar.m
@@ -2,4 +2,27 @@
 
 @implementation vstatusbar
 
+-(instancetype)init
+{
+    self = [super init];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor clearColor]];
+    
+    UIView *border = [[UIView alloc] init];
+    [border setClipsToBounds:YES];
+    [border setUserInteractionEnabled:NO];
+    [border setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [border setBackgroundColor:colormain];
+    
+    [self addSubview:border];
+    
+    NSDictionary *views = @{@"border":border};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[border]-0-|" options:0 metrics:metrics views:views]];
+    
+    return self;
+}
+
 @end
