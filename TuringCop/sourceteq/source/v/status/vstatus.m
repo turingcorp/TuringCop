2bd0cbec3533fae76d2e5580d2709aa83c8d7235
vauxhall <zero.griffin@gmail.com>
2016-02-08 15:31:02 -0600

    status bar

--- TuringCop/sourceteq/source/v/status/vstatus.m
+++ TuringCop/sourceteq/source/v/status/vstatus.m
@@ -7,7 +7,9 @@
     self = [super init:controller];
     [self setBackgroundColor:[UIColor blackColor]];
     
-    UICollectionView *collection = [[UICollectionView alloc] init];
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
     [collection setClipsToBounds:YES];
     [collection setShowsHorizontalScrollIndicator:NO];
     [collection setShowsVerticalScrollIndicator:NO];
@@ -20,6 +22,13 @@
     self.statusbar = statusbar;
     
     [self addSubview:collection];
+    [self addSubview:statusbar];
+    
+    NSDictionary *views = @{@"bar":statusbar};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[bar]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[bar(120)]" options:0 metrics:metrics views:views]];
     
     return self;
 }
--- TuringCop/sourceteq/source/v/status/vstatusbar.m
+++ TuringCop/sourceteq/source/v/status/vstatusbar.m
@@ -6,7 +6,7 @@
 {
     self = [super init];
     [self setClipsToBounds:YES];
-    [self setBackgroundColor:[UIColor clearColor]];
+    [self setBackgroundColor:[UIColor blackColor]];
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     
     UIView *border = [[UIView alloc] init];
@@ -21,7 +21,7 @@
     NSDictionary *metrics = @{};
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[border]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[border(1)]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
