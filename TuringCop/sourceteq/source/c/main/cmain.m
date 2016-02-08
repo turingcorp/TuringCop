307e68503b3cebc80678bcd3c27e5dc6f8cd2048
vauxhall <zero.griffin@gmail.com>
2016-02-08 14:24:24 -0600

    project setup

--- TuringCop/sourceteq/source/c/main/cmain.m
+++ TuringCop/sourceteq/source/c/main/cmain.m
@@ -22,12 +22,12 @@
 {
     [super viewDidLoad];
     [self.navigationBar setBackgroundColor:[UIColor blackColor]];
-    [self.navigationBar setBarTintColor:[UIColor colorWithWhite:0.4 alpha:1]];
+    [self.navigationBar setBarTintColor:[UIColor colorWithWhite:0.3 alpha:1]];
     [self.navigationBar setTintColor:[UIColor whiteColor]];
-    [self.navigationBar setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:fontboldname size:16], NSForegroundColorAttributeName:colormain}];
+    [self.navigationBar setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:fontboldname size:16], NSForegroundColorAttributeName:[UIColor whiteColor]}];
     [self.navigationBar setTranslucent:NO];
     
-    [[UIBarButtonItem appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:fontboldname size:15], NSForegroundColorAttributeName:colormain} forState:UIControlStateNormal];
+    [[UIBarButtonItem appearance] setTitleTextAttributes:@{NSFontAttributeName:[UIFont fontWithName:fontboldname size:15], NSForegroundColorAttributeName:[UIColor whiteColor]} forState:UIControlStateNormal];
 }
 
 -(UIStatusBarStyle)preferredStatusBarStyle
