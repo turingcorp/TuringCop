1fb7d01098eccd15a9bf7ce4026e65f5d6fcae35
vauxhall <zero.griffin@gmail.com>
2016-02-08 16:22:43 -0600

    view turing

--- TuringCop/sourceteq/source/v/turing/vturing.m
+++ TuringCop/sourceteq/source/v/turing/vturing.m
@@ -8,7 +8,27 @@
     [self setClipsToBounds:YES];
     [self setBackgroundColor:[UIColor blackColor]];
     
+    UIView *border = [[UIView alloc] init];
+    [border setBackgroundColor:[UIColor whiteColor]];
+    [border setUserInteractionEnabled:NO];
+    [border setTranslatesAutoresizingMaskIntoConstraints:NO];
     
+    UIButton *button = [[UIButton alloc] init];
+    [button setClipsToBounds:YES];
+    [button setTitle:NSLocalizedString(@"turing_stoptest", nil) forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor colorWithWhite:1 alpha:0.1] forState:UIControlStateHighlighted];
+    [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [button.titleLabel setFont:[UIFont fontWithName:fontname size:16]];
+    
+    [self addSubview:border];
+    [self addSubview:button];
+    
+    NSDictionary *views = @{@"border":border};
+    NSDictionary *metrics = @{};
+    
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[border]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[border(1)]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
