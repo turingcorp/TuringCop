073c8ae36bd0743fbe92bd02f99cabe10e75c067
vauxhall <zero.griffin@gmail.com>
2016-02-09 07:49:57 -0600

    model

---
+++ TuringCop/sourceteq/source/m/creator/modcreatoradditemimage.m
@@ -0,0 +1,13 @@
+//
+//  modcreatoradditemimage.m
+//  TuringCop
+//
+//  Created by zero on 2/9/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "modcreatoradditemimage.h"
+
+@implementation modcreatoradditemimage
+
+@end
---
+++ TuringCop/sourceteq/source/m/creator/modcreatoradditemtext.m
@@ -0,0 +1,13 @@
+//
+//  modcreatoradditemtext.m
+//  TuringCop
+//
+//  Created by zero on 2/9/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "modcreatoradditemtext.h"
+
+@implementation modcreatoradditemtext
+
+@end
