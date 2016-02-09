7a4adfc44c0bccc8d5f67ae90f04ebf4ef6008fe
vauxhall <zero.griffin@gmail.com>
2016-02-08 18:11:23 -0600

    creator

--- TuringCop/sourceteq/source/v/creator/vcreatorheader.m
+++ TuringCop/sourceteq/source/v/creator/vcreatorheader.m
@@ -1,6 +1,10 @@
 #import "vcreatorheader.h"
 
 @implementation vcreatorheader
+{
+    CGFloat topmargin;
+    CGFloat interitem;
+}
 
 -(instancetype)initWithFrame:(CGRect)frame
 {
@@ -9,20 +13,51 @@
     [self setBackgroundColor:[UIColor clearColor]];
     
     self.model = [[mcreatorheader alloc] init];
+    topmargin = 20;
+    interitem = 2;
     
-    UICollectionViewFlowLayout *flow
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setHeaderReferenceSize:CGSizeZero];
+    [flow setFooterReferenceSize:CGSizeZero];
+    [flow setMinimumInteritemSpacing:0];
+    [flow setMinimumLineSpacing:interitem];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    [flow setSectionInset:UIEdgeInsetsMake(topmargin, 0, 0, 0)];
     
-    NSDictionary *views = @{@"button":button};
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:flow];
+    [collection setClipsToBounds:YES];
+    [collection setBackgroundColor:[UIColor clearColor]];
+    [collection setDataSource:self];
+    [collection setDelegate:self];
+    [collection registerClass:[vcreatorheadercel class] forCellWithReuseIdentifier:celid];
+    [collection setShowsHorizontalScrollIndicator:NO];
+    [collection setShowsVerticalScrollIndicator:NO];
+    [collection setBounces:NO];
+    [collection setScrollEnabled:NO];
+    [collection setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    [self addSubview:collection];
+    
+    NSDictionary *views = @{@"col":collection};
     NSDictionary *metrics = @{};
     
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-20-[button(120)]" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[button(38)]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[col]-0-|" options:0 metrics:metrics views:views]];
     
     return self;
 }
 
 #pragma mark -
 #pragma mark col del
+
+-(CGSize)collectionView:(UICollectionView*)col layout:(UICollectionViewLayout*)layout sizeForItemAtIndexPath:(NSIndexPath*)index
+{
+    NSUInteger count = [self.model count];
+    
+    CGSize size = CGSizeMake((col.bounds.size.width - (interitem * (count - 1)))/ count, col.bounds.size.height - topmargin);
+    
+    return size;
+}
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)col
 {
