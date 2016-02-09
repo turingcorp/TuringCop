f04ea18d045c7bdc269d890ab44fbf9e22330ad1
vauxhall <zero.griffin@gmail.com>
2016-02-09 09:41:40 -0600

    model

--- TuringCop/sourceteq/source/v/creator/vcreatoraddheader.m
+++ TuringCop/sourceteq/source/v/creator/vcreatoraddheader.m
@@ -9,8 +9,14 @@
     [self setBackgroundColor:[UIColor clearColor]];
     
     UITextField *field = [[UITextField alloc] init];
+    [field setAutocapitalizationType:UITextAutocapitalizationTypeSentences];
+    [field setAutocorrectionType:UITextAutocorrectionTypeYes];
+    [field setBackgroundColor:[UIColor clearColor]];
+    self.field = field;
     
     UIStepper *stepper = [[UIStepper alloc] init];
+    
+    self.stepper = stepper;
     
     [self addSubview:field];
     [self addSubview:stepper];
