bd7c616fe5f0c791d96f99ee503d4fd3e60930ca
vauxhall <zero.griffin@gmail.com>
2016-02-09 07:59:55 -0600

    model

--- TuringCop/sourceteq/source/v/creator/vcreatoradd.h
+++ TuringCop/sourceteq/source/v/creator/vcreatoradd.h
@@ -1,6 +1,10 @@
 #import "appdel.h"
 #import "viewbase.h"
 
+@class mcreatoradd;
+
 @interface vcreatoradd:viewbase
+
+@property(strong, nonatomic)mcreatoradd *model;
 
 @end
--- TuringCop/sourceteq/source/v/creator/vcreatoradd.m
+++ TuringCop/sourceteq/source/v/creator/vcreatoradd.m
@@ -2,4 +2,15 @@
 
 @implementation vcreatoradd
 
+-(instancetype)init:(ccreatoradd*)controller
+{
+    self = [super init:controller];
+    [self setClipsToBounds:YES];
+    [self setBackgroundColor:[UIColor blackColor]];
+    
+    self.model = [[mcreatoradd alloc] init];
+    
+    return self;
+}
+
 @end
