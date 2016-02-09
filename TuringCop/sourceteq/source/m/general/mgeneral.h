905c44959fffae92876b68dbae2d603f97a9b638
vauxhall <zero.griffin@gmail.com>
2016-02-09 08:30:20 -0600

    model

--- TuringCop/sourceteq/source/m/general/mgeneral.h
+++ TuringCop/sourceteq/source/m/general/mgeneral.h
@@ -2,23 +2,31 @@
 
 // app
 
-// camera
+// creator
+
+typedef NS_ENUM(NSUInteger, test_type)
+{
+    test_type_text,
+    test_type_color,
+    test_type_image,
+    test_type_number
+};
 
 // analytics
 
-typedef NS_ENUM(NSInteger, ga_screen)
+typedef NS_ENUM(NSUInteger, ga_screen)
 {
     ga_screen_status,
     ga_screen_rate
 };
 
-typedef NS_ENUM(NSInteger, ga_event)
+typedef NS_ENUM(NSUInteger, ga_event)
 {
     ga_event_rate,
     ga_event_rate_review
 };
 
-typedef NS_ENUM(NSInteger, ga_action)
+typedef NS_ENUM(NSUInteger, ga_action)
 {
     ga_action_error,
     ga_action_start,
