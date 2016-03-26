b7a7bf3b268ad9d448333cadd4f6c8a6f99bc7b9
vauxhall <zero.griffin@gmail.com>
2016-03-26 17:07:09 -0600

    gimage

---
+++ TuringCop/sourceteq/source/g/base/gimage.h
@@ -0,0 +1,13 @@
+//
+//  gimage.h
+//  TuringCop
+//
+//  Created by zero on 3/26/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface gimage : NSObject
+
+@end
---
+++ TuringCop/sourceteq/source/g/base/gimage.m
@@ -0,0 +1,13 @@
+//
+//  gimage.m
+//  TuringCop
+//
+//  Created by zero on 3/26/16.
+//  Copyright © 2016 Iturbide. All rights reserved.
+//
+
+#import "gimage.h"
+
+@implementation gimage
+
+@end
