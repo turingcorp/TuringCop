ce9450a3242282176645c99f5189312b8f34c9b1
vauxhall <zero.griffin@gmail.com>
2016-03-28 17:33:26 -0600

    invader assets

--- TuringCop/sourceteq/source/g/foe/gfoeinvader.m
+++ TuringCop/sourceteq/source/g/foe/gfoeinvader.m
@@ -8,8 +8,8 @@
     self.model = model;
     self.x = 50;
     self.y = 50;
-    self.width = 84;
-    self.height = 84;
+    self.width = 100;
+    self.height = 100;
     [self.image loadtextures:@[@"foe_invader0", @"foe_invader1"]];
     self.image.random = YES;
     self.image.srgb = YES;
