78aac1794a0a7a9e740110780a7cb9cfabd1c161
vauxhall <zero.griffin@gmail.com>
2016-03-28 17:54:11 -0600

    invader asset

--- TuringCop/sourceteq/source/g/base/gimage.m
+++ TuringCop/sourceteq/source/g/base/gimage.m
@@ -11,7 +11,7 @@
 -(instancetype)init
 {
     self = [super init];
-    self.speed = 30;
+    self.speed = 15;
     self.textures = [NSMutableArray array];
     self.random = NO;
     self.srgb = NO;
--- TuringCop/sourceteq/source/g/foe/gfoeinvader.m
+++ TuringCop/sourceteq/source/g/foe/gfoeinvader.m
@@ -8,10 +8,10 @@
     self.model = model;
     self.x = 50;
     self.y = 50;
-    self.width = 100;
-    self.height = 100;
-    [self.image loadtextures:@[@"foe_invader0", @"foe_invader1"]];
-    self.image.random = YES;
+    self.width = 50;
+    self.height = 50;
+    [self.image loadtextures:@[@"foe_invader0", @"foe_invader1", @"foe_invader2", @"foe_invader3", @"foe_invader4", @"foe_invader5", @"foe_invader6"]];
+    self.image.random = NO;
     self.image.srgb = YES;
     [self render];
     
