30ad468920611d23325cd9e431fde8ddc2081b52
vauxhall <zero.griffin@gmail.com>
2016-03-29 09:41:24 -0600

    foe octo assets

--- TuringCop/sourceteq/source/m/gamearea/mgamearea.h
+++ TuringCop/sourceteq/source/m/gamearea/mgamearea.h
@@ -3,5 +3,11 @@
 @interface mgamearea:NSObject
 
 @property(strong, nonatomic)UIImage *image;
+@property(nonatomic)CGFloat x;
+@property(nonatomic)CGFloat y;
+@property(nonatomic)CGFloat width;
+@property(nonatomic)CGFloat height;
+@property(nonatomic)CGFloat centerx;
+@property(nonatomic)CGFloat centery;
 
 @end
--- TuringCop/sourceteq/source/m/gamearea/mgamearea.m
+++ TuringCop/sourceteq/source/m/gamearea/mgamearea.m
@@ -2,4 +2,19 @@
 
 @implementation mgamearea
 
+-(instancetype)init:(UIImage*)image
+{
+    self = [super init];
+    self.image = image;
+    
+    CGSize imagesize = image.size;
+    self.width = imagesize.width;
+    self.height = imagesize.height;
+    
+    CGFloat width_2 = self.width / 2;
+    CGFloat height_2 = self.height / 2;
+    
+    return self;
+}
+
 @end
