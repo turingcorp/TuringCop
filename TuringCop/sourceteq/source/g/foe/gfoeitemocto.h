b731da63da46bf8ee3f970a5e3929068741ef2a0
vauxhall <zero.griffin@gmail.com>
2016-03-29 08:45:04 -0600

    gfoe item octo

---
+++ TuringCop/sourceteq/source/g/foe/gfoeitemocto.h
@@ -0,0 +1,6 @@
+#import "appdel.h"
+#import "gfoeitem.h"
+
+@interface gfoeitemocto:gfoeitem
+
+@end
---
+++ TuringCop/sourceteq/source/g/foe/gfoeitemocto.m
@@ -0,0 +1,26 @@
+#import "gfoeitemocto.h"
+
+@interface gfoeitemocto ()
+
+@property(weak, nonatomic)mfoeitemocto *model;
+
+@end
+
+@implementation gfoeitemocto
+
+@dynamic model;
+
+-(instancetype)init:(mfoeitemocto*)model
+{
+    self = [super init:model];
+    self.x = 200;
+    self.y = self.realy = 200;
+    self.width = 50;
+    self.height = 50;
+    [self.image loadtextures:@[@"foe_octo0"]];
+    [self floatovershadow];
+    
+    return self;
+}
+
+@end
