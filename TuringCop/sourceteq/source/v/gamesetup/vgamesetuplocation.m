94b40041b2ed5e24d0639eb2d5ab77c358662fe2
vauxhall <zero.griffin@gmail.com>
2016-03-29 20:32:14 -0600

    location title

--- TuringCop/sourceteq/source/v/gamesetup/vgamesetuplocation.m
+++ TuringCop/sourceteq/source/v/gamesetup/vgamesetuplocation.m
@@ -10,6 +10,22 @@
     [self setTranslatesAutoresizingMaskIntoConstraints:NO];
     self.controller = controller;
     
+    NSDictionary *dicttitle = @{NSForegroundColorAttributeName:[UIColor second], NSFontAttributeName:[UIFont boldsize:15]};
+    NSDictionary *dictlocation = @{NSForegroundColorAttributeName:[UIColor main], NSFontAttributeName:[UIFont regularsize:18]};
+    NSString *stringtitle = NSLocalizedString(@"game_setup_mission_title", nil);
+    NSString *stringlocation = controller.model.venue.name;
+    
+    if(!stringlocation)
+    {
+        stringlocation = NSLocalizedString(@"game_setup_mission_unknown", nil);
+    }
+    
+    NSAttributedString *attrtitle = [[NSAttributedString alloc] initWithString:stringtitle attributes:dicttitle];
+    NSAttributedString *attrlocation = [[NSAttributedString alloc] initWithString:stringlocation attributes:dictlocation];
+    NSMutableAttributedString *mut = [[NSMutableAttributedString alloc] init];
+    [mut appendAttributedString:attrtitle];
+    [mut appendAttributedString:attrlocation];
+    
     vgamesetuplocationgradient *gradient = [[vgamesetuplocationgradient alloc] init];
     
     UIImageView *map = [[UIImageView alloc] init];
@@ -27,7 +43,7 @@
     [label setFont:[UIFont regularsize:18]];
     [label setNumberOfLines:0];
     [label setTextAlignment:NSTextAlignmentCenter];
-    [label setText:[NSString stringWithFormat:NSLocalizedString(@"game_setup_mission_title", nil), controller.model.venue.name]];
+    [label setAttributedText:mut];
     
     [self addSubview:map];
     [self addSubview:gradient];
@@ -38,8 +54,8 @@
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[map]-0-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[gradient]-0-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-50-[label]-50-|" options:0 metrics:metrics views:views]];
-    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[map]-0-[label(50)]-20-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-5-[label]-5-|" options:0 metrics:metrics views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[map]-0-[label(60)]-20-|" options:0 metrics:metrics views:views]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[gradient(50)]-0-[label]" options:0 metrics:metrics views:views]];
     
     return self;
